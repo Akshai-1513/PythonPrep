83a2ff8e5b785f857f22dad6df885fe50b312f3c
Akshai-1513 <akshaikumartn@gmail.com>
2025-01-28 10:04:16 +0530

    updated fibonacci series python file

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -1,10 +1,5 @@
 n = int(input("Enter the number: "))
 a, b = 0, 1
 for _  in range(n):
-<<<<<<< HEAD
     a, b = b, a+b # Here we add the the value of a and b to get the next value
     print(b, end = " ")
-=======
-    print(a, end = " ")
-    a, b = b, a + b
->>>>>>> f4e2406cda9f77ac143d426e70138c072f3c1d5b
